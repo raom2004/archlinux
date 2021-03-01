ac6451667749099f7e667a332cae4d6573d1273c
angel <raom2004@gmail.com>
2021-03-01 15:50:07 +0100

    testing base system 3 scripts 7

--- archlinux_installing/script2.sh
+++ archlinux_installing/script2.sh
@@ -9,7 +9,8 @@ hwclock --systohc
 localectl set-locale LANG=en_US.UTF-8
 
 ## set host config
-read -p "Enter hostname: " host_name
+# read -p "Enter hostname: " host_name
+host_name=example
 echo "$host_name" > /etc/hostname
 bash -c "echo '127.0.0.1	localhost
 ::1		localhost
@@ -33,9 +34,10 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 ## set root password and add a new user
 echo
 printf "set root password\n"
-passwd
+(echo "example") | passwd
 echo
-read -p "Enter USERNAME: " name
+# read -p "Enter USERNAME: " name
+name=$host_name
 useradd -m "$name" -s /bin/zsh
 # useradd -m $name -s /bin/zsh  # option to define shell
 echo
@@ -51,6 +53,6 @@ systemctl enable dhcpcd
 # enable desktop environment at startup
 systemctl enable lightdm
 
-sh /home/script3.sh
+# sh /home/script3.sh
 
 exit
--- archlinux_installing/script3.sh
+++ archlinux_installing/script3.sh
@@ -15,7 +15,7 @@ function aur_install {
 # locatectl --no-convert set-x11-keymap es,us pc105
 
 # localectl set-x11-keymap "es,us" pc105
-# sudo localectl set-x11-keymap "es,us" pc105
+sudo localectl set-x11-keymap "es,us" pc105
 
 # add languages to locale
 # sudo bash -c "sed -i 's/#es_ES.UTF-8/en_US.UTF-8/g' /etc/locale.gen"
@@ -28,43 +28,42 @@ function aur_install {
 # echo 'KEYMAP=es' > /etc/vconsole.conf
 # echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
-echo 'Section "InputClass"
-     Identifier "system-keyboard"
-     MatchIsKeyboard "on"
-     Option "XkbLayout" "es,us"
-     Option "XkbModel" "pc105"
-EndSection' > /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo bash -c "echo 'Section \"InputClass\"
+     Identifier \"system-keyboard\"
+     MatchIsKeyboard \"on\"
+     Option \"XkbLayout\" \"es,us\"
+     Option \"XkbModel\" \"pc105\"
+EndSection' > /etc/X11/xorg.conf.d/00-keyboard.conf"
 
 # sudo bash -c "echo 'LANG=es_ES.UTF-8
 # LC_MESSAGES=en_US.UTF-8' > /etc/locale.conf"
 
 # autologin
-# sudo bash -c "sed -i 's/#autologin-guest=false/autologin-guest=false/g;
-bash -c "sed -i 's/#autologin-guest=false/autologin-guest=false/g;
+sudo bash -c "sed -i 's/#autologin-guest=false/autologin-guest=false/g;
              	s/#autologin-user=/autologin-user=$USER/g;
     	     	s/#autologin-user-timeout=0/autologin-user-timeout=0/g'\
 		/etc/lightdm/lightdm.conf"
 
-groupadd -r autologin
+sudo groupadd -r autologin
 
-gpasswd -a "$USER" autologin
+sudo gpasswd -a "$USER" autologin
 
 # show grub menu only when shift is pressed 
-bash -c "echo '
+sudo bash -c "echo '
 GRUB_FORCE_HIDDEN_MENU=\"true\"
 # GRUB menu is hiden until you press \"shift\"' > /etc/default/grub"
 
-wget -c \
+sudo wget -c \
  'https://raw.githubusercontent.com/raom2004/arch/master/31_hold_shift' \
  --directory-prefix /etc/grub.d/
 
 # asign permissions to it  
 
-chmod a+x /etc/grub.d/31_hold_shift
+sudo chmod a+x /etc/grub.d/31_hold_shift
 
 # re-generate grub
 
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # fix wrong time in dual boot gnu/linux - windows (linux shell command) 
 
@@ -72,15 +71,15 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # set custom wallpaper 
 
-new_user=$(cat /etc/passwd | tail -n1 | awk -F':' ' { print $1 }')
+# new_user=$(cat /etc/passwd | tail -n1 | awk -F':' ' { print $1 }')
 
-wget -c \
+sudo wget -c \
  'https://raw.githubusercontent.com/raom2004/arch/master/bird.jpg' \
- --directory-prefix /home/$new_user/Pictures
+ --directory-prefix /home/$USER/Pictures
 
 
 
-gsettings set org.cinnamon.desktop.background picture-uri file:////home/$new_user/Pictures/bird.jpg
+sudo gsettings set org.cinnamon.desktop.background picture-uri file:////home/$USER/Pictures/bird.jpg
 
 # # cinnamon sound events
 
