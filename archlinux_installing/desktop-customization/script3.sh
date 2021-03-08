066a8f5d0e42ed1add6589afff38f0e0549235e4
angel <raom2004@gmail.com>
2021-03-08 11:07:20 +0100

    testing sound theme deepin

--- archlinux_installing/desktop-customization/script3.sh
+++ archlinux_installing/desktop-customization/script3.sh
@@ -188,49 +188,39 @@ sudo rm -rf ~/.config/autostart/script3.desktop
 
 
 # gsettings set org.cinnamon.desktop.sound 
-aur_install https://aur.archlinux.org/humanity-icon-theme.git
-aur_install https://aur.archlinux.org/numix-icon-theme-git.git
-aur_install https://aur.archlinux.org/numix-circle-icon-theme-git.git
+# aur_install https://aur.archlinux.org/humanity-icon-theme.git
+# aur_install https://aur.archlinux.org/numix-icon-theme-git.git
+# aur_install https://aur.archlinux.org/numix-circle-icon-theme-git.git
 
-# url="https://aur.archlinux.org/yaru.git"
-# if [[ ! -n "$(pacman -Qm yaru)" ]]; then 
-#     sudo pacman -Sy meson sassc --needed --noconfirm
-#     git clone $url /tmp/yaru
-#     cd /tmp/yaru
-#     makepkg -sri --noconfirm
-#     mkdir -p /usr/share/sounds/yaru
-#     cp -R -u -p /tmp/yaru/src/yaru-*/sounds/src/* /usr/share/sounds/yaru
-#     cd $OLDPWD
-# fi
+sudo pacman -S --needed deepin-sound-theme --noconfirm
 
-
-# ## Set Sounds (if yaru package was correctly installed)
-# if [[ -n "$(ls /usr/share/sounds/yaru)" ]]; then
-#     gsettings set org.cinnamon.desktop.sound event-sounds=true
-#     gsettings set org.cinnamon.desktop.sound volume-sound-file '/usr/share/sounds/yaru/stereo/audio-volume-change.oga'
-#     gsettings set org.cinnamon.sounds close-enabled true
-#     gsettings set org.cinnamon.sounds close-file '/usr/share/sounds/yaru/stereo/power-unplug.oga'
-#     gsettings set org.cinnamon.sounds login-enabled true
-#     gsettings set org.cinnamon.sounds login-file '/usr/share/sounds/yaru/stereo/system-ready.oga'
-#     gsettings set org.cinnamon.sounds logout-enabled true
-#     gsettings set org.cinnamon.sounds logout-file '/usr/share/sounds/yaru/stereo/device-removed.oga'
-#     gsettings set org.cinnamon.sounds map-enabled true
-#     gsettings set org.cinnamon.sounds map-file '/usr/share/sounds/yaru/stereo/power-plug.oga'
-#     gsettings set org.cinnamon.sounds maximize-enabled true
-#     gsettings set org.cinnamon.sounds maximize-file '/usr/share/sounds/yaru/stereo/power-plug.oga'
-#     gsettings set org.cinnamon.sounds minimize-enabled true
-#     gsettings set org.cinnamon.sounds minimize-file '/usr/share/sounds/freedesktop/stereo/window-attention.oga'
-#     gsettings set org.cinnamon.sounds notification-enabled true
-#     gsettings set org.cinnamon.sounds notification-file '/usr/share/sounds/yaru/stereo/desktop-login.oga'
-#     gsettings set org.cinnamon.sounds plug-enabled true
-#     gsettings set org.cinnamon.sounds plug-file '/usr/share/sounds/yaru/stereo/power-plug.oga'
-#     gsettings set org.cinnamon.sounds switch-enabled true
-#     gsettings set org.cinnamon.sounds switch-file '/usr/share/sounds/yaru/stereo/power-plug.oga'
-#     gsettings set org.cinnamon.sounds unmaximize-enabled true
-#     gsettings set org.cinnamon.sounds unmaximize-file '/usr/share/sounds/yaru/stereo/device-removed.oga'
-#     gsettings set org.cinnamon.sounds unplug-enabled true
-#     gsettings set org.cinnamon.sounds unplug-file '/usr/share/sounds/yaru/stereo/power-unplug.oga'
-# fi
+## Set Sounds (if deepin package was correctly installed)
+if [[ -n "$(ls /usr/share/sounds/deepin)" ]]; then
+    gsettings set org.cinnamon.desktop.sound event-sounds=true
+    gsettings set org.cinnamon.desktop.sound volume-sound-file '/usr/share/sounds/deepin/stereo/audio-volume-change.wav'
+    gsettings set org.cinnamon.sounds close-enabled true
+    gsettings set org.cinnamon.sounds close-file '/usr/share/sounds/deepin/stereo/power-unplug.wav'
+    gsettings set org.cinnamon.sounds login-enabled true
+    gsettings set org.cinnamon.sounds login-file '/usr/share/sounds/deepin/stereo/system-ready.wav'
+    gsettings set org.cinnamon.sounds logout-enabled true
+    gsettings set org.cinnamon.sounds logout-file '/usr/share/sounds/deepin/stereo/device-removed.wav'
+    gsettings set org.cinnamon.sounds map-enabled true
+    gsettings set org.cinnamon.sounds map-file '/usr/share/sounds/deepin/stereo/power-plug.wav'
+    gsettings set org.cinnamon.sounds maximize-enabled true
+    gsettings set org.cinnamon.sounds maximize-file '/usr/share/sounds/deepin/stereo/power-plug.wav'
+    gsettings set org.cinnamon.sounds minimize-enabled true
+    gsettings set org.cinnamon.sounds minimize-file '/usr/share/sounds/freedesktop/stereo/window-attention.wav'
+    gsettings set org.cinnamon.sounds notification-enabled true
+    gsettings set org.cinnamon.sounds notification-file '/usr/share/sounds/deepin/stereo/desktop-login.wav'
+    gsettings set org.cinnamon.sounds plug-enabled true
+    gsettings set org.cinnamon.sounds plug-file '/usr/share/sounds/deepin/stereo/power-plug.wav'
+    gsettings set org.cinnamon.sounds switch-enabled true
+    gsettings set org.cinnamon.sounds switch-file '/usr/share/sounds/deepin/stereo/power-plug.wav'
+    gsettings set org.cinnamon.sounds unmaximize-enabled true
+    gsettings set org.cinnamon.sounds unmaximize-file '/usr/share/sounds/deepin/stereo/device-removed.wav'
+    gsettings set org.cinnamon.sounds unplug-enabled true
+    gsettings set org.cinnamon.sounds unplug-file '/usr/share/sounds/deepin/stereo/power-unplug.wav'
+fi
 
 neofetch
 
