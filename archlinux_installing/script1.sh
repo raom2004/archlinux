7586227d9bb832d3a97b3389f8c71d1044552e0a
angel <raom2004@gmail.com>
2021-12-07 12:50:25 +0100

    hotfix: added missed character

--- archlinux_installing/script1.sh
+++ archlinux_installing/script1.sh
@@ -163,44 +163,44 @@ mount /dev/sda1 /mnt/boot
 pacman -Syy --noconfirm archlinux-keyring
 
 ## list of packages to be installed
-
+touch requirements.txt
 # esential packages
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 base
 base-devel
 linux
 EOF
 
 # editors
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 vim
 nano
 EOF
 
 # system tools	
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 zsh
 sudo
 git
 wget
 EOF
 # system mounting tools
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 gvfs
 EOF
 
 # network
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 dhcpcd
 EOF
 
 # wifi
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 networkmanager
 EOF
 
 # boot loader	
-cat <<EOF > requirements.txt
+cat <<EOF >> requirements.txt
 grub
 os-prober
 EOF
