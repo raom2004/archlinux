2972b11e1f5913dc1884de6d7ea6d4fcedd45fd3
angel <raom2004@gmail.com>
2021-12-08 16:48:19 +0100

    change: bashrc customization

--- archlinux_installing/script2.sh
+++ archlinux_installing/script2.sh
@@ -115,6 +115,22 @@ ExecStart=-/sbin/agetty --autologin ${user_name} --noclear %%I ${TERM}
 
 # Make colorcoding available for everyone
 cat <<EOF > /home/"${user_name}"/.bashrc
+#
+# ~/.bashrc
+#
+
+### BASH SCRIPT OPTIONS ##############################################
+
+set -o errexit  # exit if script command fails
+set -o nounset  # exit if script try to use undeclared variables
+set -o pipefail # catch failed piped commands
+# set -o xtrace   # trace what gets executed (useful for debugging)
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Make colorcoding available for everyone
+
 Black='\[\e[0;30m\]'	# Black
 Red='\[\e[0;31m\]'		# Red
 Green='\[\e[0;32m\]'	# Green
@@ -124,10 +140,42 @@ Purple='\[\e[0;35m\]'	# Purple
 Cyan='\[\e[0;36m\]'		# Cyan
 White='\[\e[0;37m\]'	# White
 
+# Bold
+BBlack='\[\e[1;30m\]'	# Black
+BRed='\[\e[1;31m\]'		# Red
+BGreen='\[\e[1;32m\]'	# Green
+BYellow='\[\e[1;33m\]'	# Yellow
+BBlue='\[\e[1;34m\]'	# Blue
+BPurple='\[\e[1;35m\]'	# Purple
+BCyan='\[\e[1;36m\]'	# Cyan
+BWhite='\[\e[1;37m\]'	# White
+
+# Background
+On_Black='\[\e[40m\]'	# Black
+On_Red='\[\e[41m\]'		# Red
+On_Green='\[\e[42m\]'	# Green
+On_Yellow='\[\e[43m\]'	# Yellow
+On_Blue='\[\e[44m\]'	# Blue
+On_Purple='\[\e[45m\]'	# Purple
+On_Cyan='\[\e[46m\]'	# Cyan
+On_White='\[\e[47m\]'	# White
+
 NC='\[\e[m\]'			# Color Reset
 
+ALERT="${BWhite}${On_Red}" # Bold White on red background
+
+
+# PERSONAL CUSTOMIZATION #############################################
+
+[[ -f ~/.bash_aliases ]] && source ~/.bash_aliases
+
 # Set prompt
-PS1="${Red}\u${NC}@\h: \w \\$ "
+# PS1="${Yellow}\u@\h${NC}: ${Blue}\w${NC} \\$ "
+# PS1="${Red}\u${NC}@\h: \w \\$ "
+PS1="${BRed}\u@\h${NC}: ${BBlue}\w${NC} \\$ "
+
+# set spell checker
+shopt -s cdspell
 EOF
 
 ## Enable Requited Services:
