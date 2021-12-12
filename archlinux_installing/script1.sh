cc998d6667b933e15d93564ad8645941a97da724
angel <raom2004@gmail.com>
2021-12-12 12:41:35 +0100

    hotfix error message in if comparisons

--- archlinux_installing/script1.sh
+++ archlinux_installing/script1.sh
@@ -162,13 +162,13 @@ read -sp "Enter USER PASSWORD: " user_password
 
 # parameters with fixed options that need to be validated
 read -p "Enter USER SHELL (e.g. bash, zsh) " user_shell
-[[ ! "${user_shell}" =~ ^([b][a]|[z])(sh)$ ]] && echo "invalid" | exit 0
+[[ ! "${user_shell}" =~ ^([b][a]|[z])(sh)$ ]] && echo "sh error" | exit 0
 read -p "Do you want autolog tty at startup?[y/N]" autolog_tty
-[[ ! "${autolog_tty}" != ^([yY]|[nN])$ ]] && echo "invalid" | exit 0
+[[ ! "${autolog_tty}" =~ ^([yY]|[nN])$ ]] && echo "err y/N" | exit 0
 
 # ask user to create a recovery partition and MBR
 read -p "Create a recovery partition?[y/N]" recovery_partition
-[[ ! "${recovery_partition}" =~ ^([yY]|[nN])$ ]] && exit 1
+[[ ! "${recovery_partition}" =~ ^([yY]|[nN])$ ]] && echo "err y/N"|exit 0
 
 
 ### SET TIME AND SYNCHRONIZE SYSTEM CLOCK
