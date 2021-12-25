c47db91cf37e1563e4e9446bfe15d56975da07e2
angel <raom2004@gmail.com>
2021-12-25 15:46:42 +0100

    hotfix: xorg packages required

--- archlinux_desktop_xfce/install-xfce.sh
+++ archlinux_desktop_xfce/install-xfce.sh
@@ -17,9 +17,10 @@ set -o xtrace      # trace & expand what gets executed (useful for debug)
 
 # install desktop and sound control
 if [[ ! -n "$(pacman -Qs xfce4)" ]]; then
-    sudo pacman -Sy --needed --noconfirm \
-	 xfce4 xfce4-pulseaudio-plugin \
-	 pavucontrol pavucontrol-qt
+  sudo pacman -Sy --needed --noconfirm \
+       xorg-server xterm \
+       xfce4 xfce4-pulseaudio-plugin \
+       pavucontrol pavucontrol-qt
 fi
 
 ### run desktop creating the dotfiles ".xinitrc" and ".serverrc":
