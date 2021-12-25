864235efef16b38c79357f28064417aa5590c077
angel <raom2004@gmail.com>
2021-12-25 09:38:08 +0100

    hotfix: aliases conflict

--- archlinux_dotfiles/bootstrap.sh
+++ archlinux_dotfiles/bootstrap.sh
@@ -19,21 +19,21 @@ git pull origin main
 # from this working directory to $HOME directory
 function doIt
 {
-	rsync --exclude ".git/" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.org" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~
-	source ~/.bashrc
+    rsync --exclude ".git/" \
+	  --exclude "bootstrap.sh" \
+	  --exclude "README.org" \
+	  --exclude "LICENSE" \
+	  -avh --no-perms . ~
+    source ~/.bashrc
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt
+    fi
 fi
 unset doIt
