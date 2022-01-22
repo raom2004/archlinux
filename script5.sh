fa0073da70ddb22435a628c7c719c28ee161d8bf
angel <raom2004@gmail.com>
2022-01-22 11:04:06 +0100

    testing scripts5.sh & script6.sh automating user config

---
+++ script5.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+#
+# script5.sh install archlinux with root & user accounts by arch-chroot  
+
+
+### BASH SCRIPT FLAGS FOR SECURITY AND DEBUGGING ###################
+
+# shopt -o noclobber # avoid file overwriting (>) but can be forced (>|)
+set +o history     # disably bash history temporarilly
+set -o errtrace    # inherit any trap on ERROR
+set -o functrace   # inherit any trap on DEBUG and RETURN
+set -o errexit     # EXIT if script command fails
+set -o nounset     # EXIT if script try to use undeclared variables
+set -o pipefail    # CATCH failed piped commands
+set -o xtrace      # trace & expand what gets executed (useful for debug)
+
+
+umount -R /mnt
+mount /dev/sda2 /mnt
+
+########################################
+# Purpose: display usage of this archlinux installer bash script 
+# Arguments: none
+########################################
+function display_usage
+{
+  printf "
+A bash script which automate Arch Linux install using chroot. 
+
+Usage: ${0##*/} [options]
+  -h|--help         display usage
+  -e|--example      set Arch Linux required variables automatically:
+		       * Host Name = \"myhost\"
+		       * Root Password = \"rootpassword\"
+		       * User Name = \"myuser\"
+		       * User Password = \"userpassword\"
+
+Example:
+  ${Green}$ bash ${0}${NC} 
+
+The install without -e option will ask for variables on demand, such as:
+  * A host name
+  * A root password
+  * A new user name and password
+
+LANGUAGE:
+  * The standard system locale is set to america english, e.g:
+    # echo \"LANG=en_US.UTF-8\" > /etc/vconsole.conf
+  * The script habilitate multiple languages with UTF-8 in locale, e.g.:
+    de_DE, en_US, en_GB, es_ES, fr_FR, ru_RU, zn_CN
+  * The standard confirguration can be overrided by per user session, e.g.:
+    # echo \"LANG=de_DE.UTF-8\" > ~/.config/locale.conf  
+  * Important: some desktops can override such per user configuration.
+"
+}
+
+
+set +o xtrace      # please do not show sensitive data
+
+## use positional arguments or declare variables hiding passwords by -sp option.
+case "$1" in
+    --example|-e)
+	host_name="myhost"
+	root_password="rootpassword"
+	user_name="myuser"
+	user_password="userpassword"
+	break
+	;;
+    --help|-h)
+	display_usage
+	exit 0
+	;;
+    *)
+	read -p "Enter hostname: " host_name
+	read -sp "Enter ROOT password: " root_password
+	read -p "Enter NEW user: " user_name
+	read -sp "Enter NEW user PASSWORD: " user_password
+esac
+echo "$host_name"
+
+# make these variables available for script2.sh
+export host_name
+export root_password
+export user_name
+export user_password
+
+set -o xtrace      # trace & expand what gets executed
+
+## set time and synchronize system clock
+timedatectl set-ntp true
+
+
+## HDD partitioning (BIOS/MBR)
+parted -s /dev/sda \
+       mklabel msdos \
+       mkpart primary ext2 0% 2% \
+       set 1 boot on \
+       mkpart primary ext4 2% 100%
+## HDD partitions formating (-F=overwrite if necessary) & mounting
+# root partition "/"
+mkfs.ext4 -F /dev/sda2
+mount /dev/sda2 /mnt
+# boot partition "/boot"
+mkfs.ext2 -F /dev/sda1
+mkdir /mnt/boot && mount /dev/sda1 /mnt/boot
+
+
+## Important: update package manager keyring before install packages
+pacman -Syy --noconfirm archlinux-keyring
+## install system packages (with support for wifi and ethernet)
+pacstrap /mnt base base-devel linux \
+	 zsh sudo vim git wget \
+	 dhcpcd \
+	 networkmanager \
+	 grub
+
+
+## generate file system table (required for boot loader)
+genfstab -L /mnt >> /mnt/etc/fstab
+
+
+# scripting inside chroot from outside: script2.sh
+# copy script2.sh to new system
+cp ./script6.sh /mnt/home
+# run script2.sh commands inside chroot
+arch-chroot /mnt bash /home/script6.sh
+# remove script2.sh after completed
+rm /mnt/home/script6.sh
+
+echo "installation finished succesfully"
---
+++ script6.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+#
+# ./script2.sh configure a new Arch Linux system and desktop
+#
+# Summary:
+# * This script contain all the commands required to config
+#   a new arch linux install and require arch-chroot.
+# * This script also create a script3.desktop autostart app which
+#   run the scrip3.sh on first boot to configure the new desktop 
+
+
+## show all the command that get executed and exit if anything fails
+set -xe
+
+
+## Time Configuration 
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+
+
+## Language Configuration (support for us, gb, dk, es, de)
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+sed -i 's/#en_GB.UTF-8/en_GB.UTF-8/' /etc/locale.gen
+sed -i 's/#en_DK.UTF-8/en_DK.UTF-8/' /etc/locale.gen
+sed -i 's/#es_ES.UTF-8/es_ES.UTF-8/' /etc/locale.gen
+sed -i 's/#de_DE.UTF-8/de_DE.UTF-8/' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8'              >  /etc/locale.conf
+echo 'LANGUAGE=en_US:en_GB:en:'      >> /etc/locale.conf
+echo 'LC_COLLATE=C'                  >> /etc/locale.conf
+echo 'LC_MESSAGES=en_US.UTF-8'       >> /etc/locale.conf
+echo 'LC_TIME=en_DK.UTF-8'           >> /etc/locale.conf
+# Keyboard Configuration (e.g. set spanish as keyboard layout)
+# localectl set-keymap --no-convert es # do not work under chroot
+echo 'KEYMAP=es'                     > /etc/vconsole.conf
+
+
+## Network Configuration
+echo "${host_name}" > /etc/hostname
+echo "127.0.0.1	localhost
+::1		localhost
+127.0.1.1	${host_name}.localdomain	${host_name}
+" >> /etc/hosts
+
+
+## Init ram filsesystem: Initramfs
+# Initramfs was run for pacstrap but must be run for LVM, encryption...:
+# mkinitcpio -P 
+
+
+## Boot loader GRUB
+# detect additional kernels or operative systems available
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+# hide boot loader at startup
+echo "GRUB_FORCE_HIDDEN_MENU=true"  >> /etc/default/grub
+# press shift to show boot loader menu at start up
+url="https://gist.githubusercontent.com/anonymous/8eb2019db2e278ba99be/raw/257f15100fd46aeeb8e33a7629b209d0a14b9975/gistfile1.sh"
+wget "${url}" -O /etc/grub.d/31_hold_shift
+chmod a+x /etc/grub.d/31_hold_shift
+# Install & Config a boot loader GRUB
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+## Accounts Config
+# sudo requires to turn on "wheel" groups
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+# set root password
+echo -e "${root_password}\n${root_password}" | (passwd root)
+# create new user and set ZSH as shell
+useradd -m "$user_name" -s /bin/zsh
+# set new user password
+echo -e "${user_password}\n${user_password}" | (passwd "${user_name}")
+# set user groups
+usermod -aG wheel,audio,optical,storage,power,network "${user_name}"
+
+
+## start services on reboot:
+systemctl enable dhcpcd		# ethernet
+systemctl enable NetworkManager	# wifi
+
+
+## shell support for: command not found
+pacman -S --noconfirm pkgfile && pkgfile -u
+## Pacman Package Manager Customization
+sed -i 's/#\(Color\)/\1/' /etc/pacman.conf
+# improve compiling time adding processors "nproc"
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
+## autologing tty
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+printf "[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin ${user_name} --noclear %%I $TERM
+" > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+
+
+### CREATE USER CONFIGURATION ########################################
+
+# set environment variables
+HOME=/home/"${user_name}"
+USER="${user_name}"
+
+# set user locale 
+mkdir -p $HOME/.config
+echo 'LANGUAGE=en_GB.UTF-8' > $HOME/.config/locale.conf
+ls -la $HOME/.config/locale.conf
+
+# config desktop on first startup
+mkdir -p $HOME/.config/autostart/
+echo '[Desktop Entry]
+Type=Application
+Name=script3
+Comment[C]=Script to config a new Desktop on first boot
+Terminal=true
+Exec=gnome-terminal -- bash -c "sudo bash /usr/bin/script3.sh; exec bash"
+X-GNOME-Autostart-enabled=true
+NoDisplay=false
+' > $HOME/.config/autostart/script3.desktop
+ls -la $HOME/.config/autostart/script3.desktop
+
+
+## create $USER standard dotfiles
+# ~/.bashrc
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.bashrc"
+wget "${url}" --output-document=$HOME/.bashrc
+# ~/.zshrc
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.zshrc"
+wget "${url}" --output-document=$HOME/.zshrc
+
+
+## create $USER CUSTOMIZED DOTFILES
+# ~/.aliases
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.aliases"
+wget "${url}" --output-document=$HOME/.aliases
+# ~/.bash_prompt
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.bash_prompt"
+wget "${url}" --output-document=$HOME/.bash_prompt
+# ~/.functions
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.functions"
+wget "${url}" --output-document=$HOME/.functions
+# ~/.vimrc
+url="https://raw.githubusercontent.com/raom2004/archlinux/master/dotfiles/.vimrc"
+wget "${url}" --output-document=$HOME/.vimrc
+# folder ~/.vim and vim plugin support
+url=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget "${url}" -P $HOME/.vim/autoload
+
+## vim config
+echo | vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
+
+ls -la $HOME
+## RECTIFY DOTFILES PERMISSIONS
+# chown -R "${user_name}":"${user_name}" $HOME/.*
+
+exit
+
+
+# emacs:
+# Local Variables:
+# sh-basic-offset: 2
+# End:
+
+# vim: set ts=2 sw=2 et:
