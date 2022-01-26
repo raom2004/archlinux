f36c888b26356c072f505159e272309d94239e23
angel <raom2004@gmail.com>
2022-01-26 17:03:13 +0100

    added git pull to dotfiles/bootstrap.sh

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -4,13 +4,13 @@
 #  synchronize dotfile from working directory to $HOME/
 # Requirements: rsync
 if ! pacman -Qs rsync; then 
-    echo "rsync: Command not found but required."
-    read -p "Do you want to install rsync? (Y/n) " -n 1
-    echo ""
-    if [[ ! "$REPLY" =~ ^([nN][oO]|[nN])$ ]]; then
-	sudo pacman -Syu --needed --noconfirm \
-	     rsync 
-    fi
+  echo "rsync: Command not found but required."
+  read -p "Do you want to install rsync? (Y/n) " -n 1
+  echo ""
+  if [[ ! "$REPLY" =~ ^([nN][oO]|[nN])$ ]]; then
+    sudo pacman -Syu --needed --noconfirm \
+	 rsync 
+  fi
 fi
 
 # Set the actual working directory
@@ -23,23 +23,25 @@ git pull origin main
 # from this working directory to $HOME directory
 function doIt
 {
-    rsync --exclude ".git/" \
-	  --exclude "bootstrap.sh" \
-	  --exclude "README.org" \
-	  --exclude "LICENSE" \
-	  -avh --no-perms . ~
+  rsync --exclude ".git/" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.org" \
+	--exclude "LICENSE" \
+	-avh --no-perms . ~
 
-    source "$HOME"/.bashrc
+  source "$HOME"/.bashrc
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
+  git pull
+  doIt
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo ""
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-	doIt
-    fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo ""
+  if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    git pull
+    doIt
+  fi
 fi
 unset doIt
 
