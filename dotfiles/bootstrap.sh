c47a22f3271313b3057a2ff28c9c27b762aa7509
angel <raom2004@gmail.com>
2022-01-27 19:22:54 +0100

    hotfix: wrong double parens bootstrap.sh

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -32,7 +32,7 @@ function doIt
   # source "$HOME"/.bashrc	# source .bashrc in script do not works
 }
 
-if [[ "$1" == "--force" -o "$1" == "-f" ]]; then
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
