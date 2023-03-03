c619558d4fa3fecf7362b6b8e884a1a14074ccc1
Ricardo <raom2004@gmail.com>
2023-03-03 08:54:55 +0100

    hotfix: improved previous dot-files deletion

--- script1.sh
+++ script1.sh
@@ -384,18 +384,24 @@ if find /mnt/home/"${user_name}" -maxdepth 1 -type f -name ".*"; then
   printf "\n"
   read -p "==> Dir /home detected. Delete previous configuration files?[y/N]" answer
   if [[ "${answer:-N}" =~ ^([yY])$ ]]; then
-    printf " --> Deleting 'folders' in /home/%s\n\n" "${user_name}"
-    # rm -rf /mnt/home/"${user_name}"/{bin,Downloads,Projects} || die
-    find /mnt/home/"${user_name}"/*/ -maxdepth 0 -type d \
-     -not -path "/mnt/home/${user_name}/Documents/*" \
-     -not -path "/mnt/home/${user_name}/Pictures/*" \
-     -not -path "/mnt/home/${user_name}/Videos/*" | xargs rm -rf || die
-    printf " --> Deleting 'dot-file folders' in /home/%s\n\n" "${user_name}"
+    printf " --> Deleting 'dot-files directories' in '/home' \n\n"
     find /mnt/home/"${user_name}"/ -maxdepth 1 -type d -name ".*" \
       | xargs rm -rf \
       || die
-    printf " --> Deleting 'folders' in / \n\n"
-    rm -rf /mnt/{bin,boot,dev,etc,lib,lib64,opt,run,sbin,srv,tmp,usr,var} \
+    printf " --> Deleting 'dot-files' in '/home' \n\n"
+    find /mnt/home/"${user_name}" -maxdepth 1 -type f -name ".*" \
+      | xargs rm -rf \
+      || die
+    printf " --> Deleting 'directories' in '/home' \n\n"
+    find /mnt/home/"${user_name}"/*/ -maxdepth 0 -type d \
+     -not -path "/mnt/home/${user_name}/Documents/*" \
+     -not -path "/mnt/home/${user_name}/Pictures/*" \
+     -not -path "/mnt/home/${user_name}/Videos/*" \
+      | xargs rm -rf \
+      || die
+    printf " --> Deleting 'folders' in '/' \n\n"
+    rm -rf \
+       /mnt/{bin,boot,dev,etc,lib,lib64,opt,run,sbin,srv,tmp,usr,var} \
       || die
   fi
 fi
